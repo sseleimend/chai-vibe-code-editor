18791a2d06a58d004efb9d1cd886355f77220fa6
Victor Seleimend <sseleimend@gmail.com>
2025-09-07 17:17:13 -0300

    refactor: simplify authentication middleware by removing public route checks

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,21 @@
-import { DEFAULT_LOGIN_REDIRECT, publicRoutes, authRoutes } from "@/routes";
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
+
+export const protectedRoutes: string[] = ["/"];
+export const authRoutes: string[] = ["/auth/sign-in"];
 
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isProtectedRoute = protectedRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   if (isAuthRoute && isLoggedIn) {
-    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    return NextResponse.redirect(new URL("/", nextUrl));
   }
 
-  if (isAuthRoute) {
-    return null;
-  }
-
-  if (!isLoggedIn && !isPublicRoute) {
+  if (isProtectedRoute && !isLoggedIn) {
     return NextResponse.redirect(new URL("/auth/sign-in", nextUrl));
   }
 

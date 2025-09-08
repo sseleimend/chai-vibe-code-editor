214377bb99248f7e3d271f3f504cf8ac04c28288
Victor Seleimend <sseleimend@gmail.com>
2025-09-07 21:36:16 -0300

    feat: add authentication configuration with GitHub and Google providers

---
+++ auth.config.ts
@@ -0,0 +1,16 @@
+import { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+export const authConfig = {
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+} satisfies NextAuthConfig;

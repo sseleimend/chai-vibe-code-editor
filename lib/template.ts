3aaf33a036b6bdf545a01acdab3b60cb2bd3f15b
Victor Seleimend <sseleimend@gmail.com>
2025-09-13 18:40:41 -0300

    feat: add templatePaths for various starter templates

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+  REACT: "/starters/react-ts",
+  NEXT: "/starters/nextjs",
+  EXPRESS: "/starters/express-simple",
+  VUE: "/starters/vue",
+  HONO: "/starters/hono-nodejs-starter",
+  ANGULAR: "/starters/angular",
+};

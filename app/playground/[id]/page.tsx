91efd8bd025ec0815b7c4c603447f9aa74c77cfe
Victor Seleimend <sseleimend@gmail.com>
2025-09-14 12:36:11 -0300

    feat: integrate file selection functionality in Page component

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -3,8 +3,11 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { TemplateFileTree } from "@/modules/playground/components/playground-explorer";
+import { useFileExplorer } from "@/modules/playground/hooks/useFileExplorer";
 import { usePlayground } from "@/modules/playground/hooks/usePlayground";
+import { TemplateFile } from "@/modules/playground/lib/path-to-json";
 import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
 function Page() {
   const { id } = useParams<{ id: string }>();
@@ -18,14 +21,40 @@ function Page() {
     saveTemplateData,
   } = usePlayground(id);
 
-  const selectedFile = "sample.txt";
+  const {
+    selectedFileId,
+    closeAllFiles,
+    openFile,
+    openFiles,
+    setTemplateData,
+    setSelectedFileId,
+    setPlaygroundId,
+    setOpenFiles,
+  } = useFileExplorer();
+
+  useEffect(() => {
+    setPlaygroundId(id);
+  }, [id, setPlaygroundId]);
+
+  useEffect(() => {
+    if (templateData && !openFiles.length) {
+      setTemplateData(templateData);
+    }
+  }, [templateData, setTemplateData, openFiles.length]);
+
+  const selectedFile = openFiles.find((f) => f.id === selectedFileId);
+  const hasUnsavedChanges = openFiles.some((f) => f.hasUnsavedChanges);
+
+  const handleFileSelect = (file: TemplateFile) => {
+    openFile(file);
+  };
 
   return (
     <TooltipProvider>
       <>
         <TemplateFileTree
           data={templateData!}
-          onFileSelect={() => {}}
+          onFileSelect={handleFileSelect}
           selectedFile={selectedFile}
           title="File Explorer"
           onAddFile={() => {}}

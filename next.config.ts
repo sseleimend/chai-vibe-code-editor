785e0893ff3f02f9bd0d0cf9e35843520b8eb426
Victor Seleimend <sseleimend@gmail.com>
2025-09-14 12:35:58 -0300

    feat: update image configuration to use remote patterns for improved flexibility

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
   },
 };
 

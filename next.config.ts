51d71c0c80a6a8e0c46aea54e498260517cc8980
Victor Seleimend <sseleimend@gmail.com>
2025-09-18 23:27:32 -0300

    feat: update image remote patterns in next.config.ts for improved asset loading

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,15 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "avatars.githubusercontent.com",
         port: "",
-        pathname: "/images/**",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },

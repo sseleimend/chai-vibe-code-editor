929e8779cf778bf57326b04a988ed01f3291b372
Victor Seleimend <sseleimend@gmail.com>
2025-09-20 20:03:27 -0300

    feat: add custom headers for Cross-Origin policies in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,26 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
+
+  reactStrictMode: false,
 };
 
 export default nextConfig;
